71571a6da1af1e63e5218b58aba314946e4fb27b
Stepan Volkov <stepan2volkov@yandex.ru>
2021-08-17 13:45:06 +0500

    broadcasting from stdin implemented

--- lesson2/timetick/timesrv/main.go
+++ lesson2/timetick/timesrv/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"log"
@@ -12,7 +13,7 @@ import (
 )
 
 func main() {
-	ctx, _ := signal.NotifyContext(context.Background(), os.Interrupt) //nolint
+	ctx, _ := signal.NotifyContext(context.Background(), os.Interrupt)
 
 	cfg := net.ListenConfig{
 		KeepAlive: time.Minute,
@@ -22,15 +23,23 @@ func main() {
 		log.Fatal(err)
 	}
 	wg := &sync.WaitGroup{}
-	log.Println("im started!")
+	stdin := make(chan string, 1)
+	entering := make(chan net.Conn, 1)
+	leaving := make(chan net.Conn, 1)
+	go watchStdin(ctx, stdin)
+	go broadcastServerMsg(ctx, entering, leaving, stdin)
+	log.Println("I'm started!")
 
 	go func() {
-		conn, err := l.Accept()
-		if err != nil {
-			log.Println(err)
-		} else {
-			wg.Add(1)
-			go sendTime(ctx, conn, wg)
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				return
+			} else {
+				wg.Add(1)
+				entering <- conn
+				go sendTime(ctx, conn, wg, leaving)
+			}
 		}
 	}()
 
@@ -42,7 +51,7 @@ func main() {
 
 }
 
-func sendTime(ctx context.Context, conn net.Conn, wg *sync.WaitGroup) {
+func sendTime(ctx context.Context, conn net.Conn, wg *sync.WaitGroup, leaving chan<- net.Conn) {
 	defer wg.Done()
 	defer conn.Close()
 	// каждую 1 секунду отправлять клиентам текущее время сервера
@@ -54,9 +63,40 @@ func sendTime(ctx context.Context, conn net.Conn, wg *sync.WaitGroup) {
 		case t := <-tck.C:
 			_, err := fmt.Fprintf(conn, "now: %s\n", t)
 			if err != nil {
-				log.Println(err)
+				leaving <- conn
 				return
 			}
 		}
+	}
+}
+
+func broadcastServerMsg(ctx context.Context, entering <-chan net.Conn, leaving <-chan net.Conn, stdin <-chan string) {
+	clients := make(map[net.Conn]bool)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case cli := <-entering:
+			clients[cli] = true
+		case cli := <-leaving:
+			delete(clients, cli)
+		case msg := <-stdin:
+			for cli := range clients {
+				fmt.Fprintf(cli, "MESSAGE FROM SERVER: %s", msg)
+			}
+		}
+	}
+}
+
+func watchStdin(ctx context.Context, stdin chan<- string) {
+	in := bufio.NewReader(os.Stdin)
+	for {
+		serverMsg, err := in.ReadString('\n')
+		if err != nil {
+			log.Println(err)
+			close(stdin)
+			return
+		}
+		stdin <- serverMsg
 	}
 }
