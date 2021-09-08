8961b7ab9e9d5a996aa712052e03fe4dfd6628dc
Stepan Volkov <stepan2volkov@yandex.ru>
2021-09-08 21:01:15 +0500

    unused test deleted

--- lesson4/main_test.go
+++ lesson4/main_test.go
@@ -51,28 +51,3 @@ func TestUploadHandler(t *testing.T) {
 			rr.Body.String(), expected)
 	}
 }
-
-func TestGetFileListHandler(t *testing.T) {
-	type fields struct {
-		FileSearcher Searcher
-	}
-	type args struct {
-		w http.ResponseWriter
-		r *http.Request
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			h := &GetFileListHandler{
-				FileSearcher: tt.fields.FileSearcher,
-			}
-			h.ServeHTTP(tt.args.w, tt.args.r)
-		})
-	}
-}
